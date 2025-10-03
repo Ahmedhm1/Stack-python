565a0e520c729c683df493000e464c601b9b29cd
Ahmed Hisham <139140231+Ahmedhm1@users.noreply.github.com>
2025-10-03 15:04:30 +0300

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="stack",
+    version="0.1.0",
+    description="A simple Stack (LIFO) data structure implementation in Python",
+    author="Ahmedhm1",
+    license="MIT",
+    packages=find_packages(),
+)
